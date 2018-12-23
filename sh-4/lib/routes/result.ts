00cf69e0b478fb2777a0547c2d02886382ea3f6f
tatatatam <tam_onjoy@hotmail.com>
2018-12-23 18:16:51 +0700

    add

--- sh-4/lib/routes/result.ts
+++ sh-4/lib/routes/result.ts
@@ -59,7 +59,7 @@ export const resultList = (req: Request, res: Response) => {
   const project_id = req.query.project;
   const audit_id = req.query.audit;
   const assessment_id = req.query.assessment;
-  url = "https://ananda365.sharepoint.com/sites/SmartQualityAssurance/_api/lists/getbytitle('SQA_AUDIT_RESULT')/items?$top=2000&$select=Title,Project/Title,Audit/Title,Assmnt_Type/Title,Assmnt_Category/Title,Assmnt_Subcategory/Title,Assmnt_Topic/Title,Created,Author/Title,Weight,Point,Score,Remarks&$expand=Project,Audit,Assmnt_Type,Assmnt_Category,Assmnt_Subcategory,Assmnt_Topic,Author&$filter="
+  url = "https://ananda365.sharepoint.com/sites/SmartQualityAssurance/_api/lists/getbytitle('SQA_AUDIT_RESULT')/items?$top=2000&$select=Title,Project/Title,Audit/Title,Assmnt_Type/Title,Assmnt_Category/Title,Assmnt_Subcategory/Title,Assmnt_Topic/Title,Created,Author/Title,Weight,Point,Score,Description,Remarks&$expand=Project,Audit,Assmnt_Type,Assmnt_Category,Assmnt_Subcategory,Assmnt_Topic,Author&$filter="
   let sumString = "";
   if (project_id != 0) sumString += " Project/ID eq " + project_id;
   if (audit_id != 0) sumString += " and Audit/ID eq " + audit_id;
