6991bca29b456755934ee3ffc1cf198add09c1ec
tatatatam <tam_onjoy@hotmail.com>
2019-01-27 12:27:22 +0700

    add

--- sh-3/views/endpoint.js
+++ sh-3/views/endpoint.js
@@ -1,5 +1,5 @@
 var url_dev = "http://localhost:3000/node/shananda/sh-3";
-var url_prod = "http://35.240.149.115:3001";
+var url_prod = "http://119.59.127.34:8080/node/shananda/sh-3";
 var trigger = 1;
 if (location.hostname == "localhost") trigger = 0;
 var url = trigger === 0 ? url_dev : url_prod;
