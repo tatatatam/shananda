1587143e814d8fccaf56bbc2d7a4dd1769d5020b
tatatatam <tam_onjoy@hotmail.com>
2019-01-29 23:09:12 +0700

    add

--- sh-3/views/endpoint.js
+++ sh-3/views/endpoint.js
@@ -1,6 +1,6 @@
 var url_dev = "http://localhost:3000/node/shananda/sh-3";
 var url_prod_window = "http://119.59.127.34:8080/node/shananda/sh-3";
-var url_prod_linux ="http://35.240.149.115:8080/node/shananda/sh-3"
+var url_prod_linux ="http://35.240.149.115:3001/node/shananda/sh-3"
 var trigger = 1;
 if (location.hostname == "119.59.127.34") {
   url = url_prod_window
--- sh-4/views/endpoint.js
+++ sh-4/views/endpoint.js
@@ -1,6 +1,6 @@
 var url_dev = "http://localhost:3000/node/shananda/sh-4";
 var url_prod_window = "http://119.59.127.34:8080/node/shananda/sh-4";
-var url_prod_linux ="http://35.240.149.115:8080/node/shananda/sh-4"
+var url_prod_linux ="http://35.240.149.115:3002/node/shananda/sh-4"
 var trigger =2 ;
 if (location.hostname == "119.59.127.34") {
   url = url_prod_window
